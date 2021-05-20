bd941e7a1e57733f4d6eea49839d2efc6e599410
François Proulx <francois.proulx@gmail.com>
2021-05-20 15:43:30 -0400

    Added crypto signature verif using gov prod key

--- dump_shc.js
+++ dump_shc.js
@@ -15,44 +15,65 @@ if (input === undefined) {
   process.exit(-1);
 }
 
-imageData = PNG.sync.read(fs.readFileSync(input));
+jose.JWK.asKeyStore({
+  keys: [
+    {
+      kid: "o-kjQwit6lT5I1aSzWd26SdN3FANWtwUyxKuiyEXP68",
+      alg: "ES256",
+      kty: "EC",
+      crv: "P-256",
+      use: "sig",
+      x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
+      y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0",
+    },
+  ],
+}).then(function (keystore) {
+  imageData = PNG.sync.read(fs.readFileSync(input));
 
-const scannedQR = jsQR(
-  new Uint8ClampedArray(imageData.data.buffer),
-  imageData.width,
-  imageData.height
-);
+  const scannedQR = jsQR(
+    new Uint8ClampedArray(imageData.data.buffer),
+    imageData.width,
+    imageData.height
+  );
 
-const scannedJWS = scannedQR.chunks
-  .filter((chunk) => chunk.type === "numeric")[0]
-  .text.match(/(..?)/g)
-  .map((num) => String.fromCharCode(parseInt(num, 10) + 45))
-  .join("");
+  const scannedJWS = scannedQR.chunks
+    .filter((chunk) => chunk.type === "numeric")[0]
+    .text.match(/(..?)/g)
+    .map((num) => String.fromCharCode(parseInt(num, 10) + 45))
+    .join("");
 
-console.log("QR-Code JWS Payload");
-console.log("-----");
-console.log(scannedJWS);
-console.log("-----");
+  console.log("QR-Code payload --> JWS");
+  console.log("-----");
+  console.log(scannedJWS);
+  console.log("-----");
 
-console.log("JWS Header");
-console.log(base64url.decode(scannedJWS.split(".")[0]));
-console.log("-----");
+  console.log("JWS Header");
+  console.log(base64url.decode(scannedJWS.split(".")[0]));
+  console.log("-----");
 
-const unverifiedPayload = scannedJWS.split(".")[1];
-console.log("JWS Payload (Base64)");
-console.log(unverifiedPayload);
-console.log("-----");
+  jose.JWS.createVerify(keystore)
+    .verify(scannedJWS)
+    .then(function (result) {
+      const verifiedPayload = scannedJWS.split(".")[1];
+      console.log("JWS Payload (Base64)");
+      console.log(verifiedPayload);
+      console.log("-----");
 
-const decodedPayload = Buffer.from(unverifiedPayload, "base64");
-zlib.inflateRaw(decodedPayload, function (err, decompressedResult) {
-  if (typeof err === "object" && err) {
-    console.log("Unable to decompress");
-  } else {
-    scannedResult = decompressedResult.toString("utf8");
-    const entries = JSON.parse(
-      scannedResult
-    ).vc.credentialSubject.fhirBundle.entry.map((entry) =>
-      console.log(JSON.stringify(entry, null, 2))
-    );
-  }
+      const decodedPayload = Buffer.from(verifiedPayload, "base64");
+      zlib.inflateRaw(decodedPayload, function (err, decompressedResult) {
+        if (typeof err === "object" && err) {
+          console.log("Unable to decompress");
+        } else {
+          scannedResult = decompressedResult.toString("utf8");
+          const entries = JSON.parse(
+            scannedResult
+          ).vc.credentialSubject.fhirBundle.entry.map((entry) =>
+            console.log(JSON.stringify(entry, null, 2))
+          );
+        }
+      });
+    })
+    .catch(function (e) {
+      console.log("Ooooh crap - this looks like a fake vacinnation proof");
+    });
 });
