16a4136b51869a27f675f2cd6d6cdb165699841b
Steven Luo <steven@steven676.net>
2021-06-18 23:59:38 -0700

    Add signing key for California Digital COVID-19 Vaccine Record
    Key from https://myvaccinerecord.cdph.ca.gov/creds/.well-known/jwks.json:
    
    $ sha256sum jwks.json
    eb5d4bbea0734c205daf49c03e3013071c9edfd6232d24451fdfc27c6637df5f  jwks.json

--- src/issuers.js
+++ src/issuers.js
@@ -9,6 +9,16 @@ const issuers = [
         y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0" },
     ]
   },
+  {
+    id: "us.ca",
+    iss: "https://myvaccinerecord.cdph.ca.gov/creds",
+    keys: [
+      { kid: "7JvktUpf1_9NPwdM-70FJT3YdyTiSe2IvmVxxgDSRb0",
+        alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
+        x: "3dQz5ZlbazChP3U7bdqShfF0fvSXLXD9WMa1kqqH6i4",
+        y: "FV4AsWjc7ZmfhSiHsw2gjnDMKNLwNqi2jMLmJpiKWtE" },
+    ]
+  },
 ];
 
 module.exports = {
