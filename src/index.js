d1705855ff0450a01786298bedd57f59bdb09337
Mathieu St-Vincent <m@mats.dev>
2021-08-28 23:23:53 -0400

    Added html structure, copy to clipboard and manual entry from text input

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 const ZXing = require("@zxing/library");
 const { verifyJWS, decodeJWS, getScannedJWS } = require("./shc");
 
+const resultEl = document.getElementById('result');
+
 function setResult(result) {
-  document.getElementById("result").textContent = result;
+  resultEl.value = result;
 }
 
 function decodeOnce(codeReader, selectedDeviceId) {
-  codeReader.decodeFromInputVideoDevice(selectedDeviceId, "video").then(
-    (result) => {
+  codeReader.decodeFromInputVideoDevice(selectedDeviceId, "video")
+    .then((result) => {
       console.log("SHC string", result.text);
       const scannedJWS = getScannedJWS(result.text);
       console.log("scannedJWS", scannedJWS);
-      verifyJWS(scannedJWS).then(
-        function () {
-          console.log("scannedJWS", scannedJWS);
-          return decodeJWS(scannedJWS).then((decoded) =>
-            setResult(JSON.stringify(decoded, null, 2))
-          );
-        },
-        function (e) {
-          console.error(e);
-          setResult("This looks like a fake vaccination proof");
-        }
-      );
-    },
-    (err) => {
-      setResult(err);
-    }
-  );
+
+      return verifyJWS(scannedJWS)
+        .then(() => decodeJWS(scannedJWS))
+        .then((decoded) => setResult(JSON.stringify(decoded, null, 2)))
+        .catch((error) => setResult('This looks like a fake vaccination proof'));
+    })
+    .catch((err) => setResult(err));
 }
 
 let selectedDeviceId;
@@ -69,3 +61,24 @@ codeReader
   .catch((err) => {
     console.error(err);
   });
+
+const manualEntryForm = document.forms['manual-entry'];
+manualEntryForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const shcString = formData.get('shcString');
+
+  if (shcString === '') setResult('Error: Missing manual entry input value');
+
+  const scannedJWS = getScannedJWS(shcString);
+
+  return verifyJWS(scannedJWS)
+    .then(() => decodeJWS(scannedJWS))
+    .then((decoded) => setResult(JSON.stringify(decoded, null, 2)))
+    .catch((error) => setResult('This looks like a fake vaccination proof'));
+});
+
+const copyButton = document.getElementById('copy');
+copyButton.addEventListener('click', () => navigator.clipboard.writeText(resultEl.value));
+
