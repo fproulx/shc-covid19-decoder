b188683f7ebba48fa31d333598e0554869ab7450
Steven Luo <steven@steven676.net>
2021-09-12 22:15:58 -0700

    Update Quebec vaccination proof iss and kid
    Thanks to Raymond Ménard for these values.

--- src/issuers.js
+++ src/issuers.js
@@ -1,9 +1,9 @@
 const issuers = [
   {
     id: "ca.qc",
-    iss: "XXX",
+    iss: "https://covid19.quebec.ca/PreuveVaccinaleApi/issuer",
     keys: [
-      { kid: "some-kid",
+      { kid: "qFdl0tDZK9JAWP6g9_cAv57c3KWxMKwvxCrRVSzcxvM",
         alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
         x: "XSxuwW_VI_s6lAw6LAlL8N7REGzQd_zXeIVDHP_j_Do",
         y: "88-aI4WAEl4YmUpew40a9vq_w5OcFvsuaKMxJRLRLL0" },
